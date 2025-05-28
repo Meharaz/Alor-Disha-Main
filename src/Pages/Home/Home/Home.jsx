9827bc8e85b873929d9b0fc859e44cb1e7a36b64
noman <ariyan.sacc@gmail.com>
2025-05-28 19:34:21 +0600

    Auto slider added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BannerSection from '../BannerSection/BannerSection';
+import Autoslider from '../AutoSlider/Autoslider';
 
 
 const Home = () => {
     return (
         <div>
             <BannerSection/>
+            <Autoslider/>
             <p>This is Home</p>
         </div>
     );
